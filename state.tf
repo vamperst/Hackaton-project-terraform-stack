e7cd124e260bdf988832a642af1b1080d5a7cf8b
Ubuntu <ubuntu@ip-172-31-74-157.ec2.internal>
2020-09-06 14:45:35 +0000

    feat(tf): Project initial commit

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-1dvp-12345"
+    bucket = "hackathon-fiap-2dvp-336387"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
